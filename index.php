003842a9489057e71ad64eff25b59dd1e66c909a
herve-beziat <herve.beziat@laplateforme.io>
2023-05-03 15:10:10 +0200

    faker ok et bdd ok

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
+
 //*                    // Match all request URIs
 //[i]                  // Match an integer
 //[i:id]               // Match an integer as 'id'
@@ -27,6 +28,36 @@ $router->map('GET', '/', function(){
 $router->map('GET', '/users', function(){
     echo "<h1>Bienvenue sur la liste des utilisateurs</h1>";
 }, 'users');
+
+//Route pour créer des users dans la bdd
+$router->map('GET', '/users/fill', function(){
+    $faker = Faker\Factory::create("fr_FR");
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=superweek", "root", "");
+        echo "connexion réussie";
+    } catch (PDOException $e) {
+        die ('ERREUR :' . $e->getMessage());
+    }
+    $queryString = "INSERT INTO `user`(`email`, `password`, `first_name`, `last_name`) VALUES (:email, :password, :firstname, :lastname)";
+    $statement = $db->prepare($queryString);
+    // var_dump($statement);
+    
+    for ($i = 0; $i < 30; $i++) {
+        $firstname = $faker->firstName();
+        $lastname = $faker->lastName();
+        $statement->execute([
+            'email' => strtolower("$firstname.$lastname@{$faker->freeEmailDomain}"),
+            'password' => password_hash('azerty', PASSWORD_DEFAULT),
+            'firstname' => $firstname,
+            'lastname' => $lastname
+        ]);
+    }
+    
+    
+    
+    
+
+}, 'create_users');
 
 // Route pour une page utilisateur spécifique
 $router->map('GET', '/users/[i:id]', function($id) {
